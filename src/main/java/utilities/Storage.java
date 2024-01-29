2dbb9e0ecea9a4951830595f9d9baa23b18a44a4
UdhayaShan1 <udhaya.shan1@gmail.com>
2024-01-29 15:08:35 +0800

    That didnt work out too well

--- src/main/java/utilities/Storage.java
+++ src/main/java/utilities/Storage.java
@@ -6,11 +6,13 @@ import tasks.Events;
 import tasks.Task;
 import tasks.Todo;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
@@ -33,7 +35,10 @@ public class Storage {
      * @throws RyanGoslingException  If the task data on the hard drive is not in the expected format.
      */
     public ArrayList<Task> parseAndLoadTasks() throws FileNotFoundException, RyanGoslingException {
-        File f = new File(filePath);
+        InputStream f = getClass().getClassLoader().getResourceAsStream(filePath);
+        if (f == null) {
+            throw new RyanGoslingException("File not found: " + filePath);
+        }
         Scanner s = new Scanner(f);
         String pattern = "^\\s*(\\w+)\\s*\\|\\s*(\\w+)\\s*\\|\\s*(.*?)\\s*\\|\\s*(.*?)\\s*\\|\\s*(.*?)\\s*$";
         ArrayList<Task> listOfTasks = new ArrayList<>();
@@ -71,23 +76,35 @@ public class Storage {
      * @param taskList The ArrayList of Task objects to be written to the hard drive.
      */
     public void writeToTaskList(ArrayList<Task> taskList) {
-        StringBuilder toAdd = new StringBuilder();
-        for (int i = 0; i < taskList.size(); i += 1) {
-            Task taskToAdd = taskList.get(i);
-            toAdd.append(taskToAdd.getTaskType());
-            int taskDone = taskToAdd.isTaskDone() ? 1 : 0;
-            String[] possibleTimes = taskToAdd.getTimes();
-            toAdd.append(" | ").append(taskDone).append(" | ").append(taskToAdd.getTaskName())
-                    .append(" | ").append(possibleTimes[0]).append(" | ").append(possibleTimes[1]);
-            if (i != taskList.size() - 1) {
-                toAdd.append(System.lineSeparator());
-            }
+        URL resourceUrl = getClass().getClassLoader().getResource(filePath);
+
+        if (resourceUrl == null) {
+            System.out.println("Resource not found: " + filePath);
+            return;
         }
+
         try {
-            FileWriter fw = new FileWriter(this.filePath);
-            fw.write(toAdd.toString());
-            fw.close();
-        } catch (IOException e) {
+            File file = new File(resourceUrl.toURI());
+            Path path = Paths.get(file.getPath());
+
+            try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+                for (int i = 0; i < taskList.size(); i += 1) {
+                    Task taskToAdd = taskList.get(i);
+                    writer.write(taskToAdd.getTaskType());
+
+                    int taskDone = taskToAdd.isTaskDone() ? 1 : 0;
+                    String[] possibleTimes = taskToAdd.getTimes();
+
+                    writer.write(" | " + taskDone + " | " + taskToAdd.getTaskName() +
+                                         " | " + possibleTimes[0] + " | " + possibleTimes[1]);
+
+                    if (i != taskList.size() - 1) {
+                        writer.newLine();
+                    }
+                }
+            }
+        } catch (IOException | URISyntaxException e) {
+            System.out.println(e.getMessage());
             System.out.println("Error writing! Weird as f");
         }
     }
